28ad9df6e11c123897546332c2308b6aec6a4821
Elmar Zander <elmar.zander@googlemail.com>
2026-04-23 16:30:56 +0200

    Rename SKI Combinators to SKI Calculus; expand with boolean defs

--- src/data/content.ts
+++ src/data/content.ts
@@ -64,7 +64,7 @@ export const EXAMPLES: { label: string; src: string }[] = ordered(exampleMap, "e
   "List Operations",
   "Tuples",
   "Recursion",
-  "SKI Combinators",
+  "SKI Calculus",
 ]);
 
 export const TUTORIALS: { label: string; src: string }[] = ordered(tutorialMap, "tutorial", [
